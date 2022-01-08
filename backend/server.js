94e827e9575a7196281f851a0caad2610b513b9b
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2022-01-08 23:02:25 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 import saleRouter from "./routers/saleRouter.js";
 import cors from "cors";
 //fake data
-import Sale from "./models/saleModel.js";
-import Code from "./models/codeModel.js";
-import { codes, sales } from "../sale.js";
+// import Sale from "./models/saleModel.js";
+// import Code from "./models/codeModel.js";
+// import { codes, sales } from "../sale.js";
 
 const app = express();
 app.use(express.json());
@@ -23,19 +23,19 @@ const port = process.env.PORT || 5000;
 app.use("/api/sale", saleRouter);
 
 //insertmany module
-app.post("/insertmany/sale", async (req, res) => {
-  try {
-    const insertMany = await Sale.insertMany(sales, function (error, docs) {});
-    res.status(201).send({ message: "Created" });
-  } catch (error) {}
-});
+// app.post("/insertmany/sale", async (req, res) => {
+//   try {
+//     const insertMany = await Sale.insertMany(sales, function (error, docs) {});
+//     res.status(201).send({ message: "Created" });
+//   } catch (error) {}
+// });
 
-app.post("/insertmany/code", async (req, res) => {
-  try {
-    const insertMany = await Code.insertMany(codes, function (error, docs) {});
-    res.status(201).send({ message: "Created" });
-  } catch (error) {}
-});
+// app.post("/insertmany/code", async (req, res) => {
+//   try {
+//     const insertMany = await Code.insertMany(codes, function (error, docs) {});
+//     res.status(201).send({ message: "Created" });
+//   } catch (error) {}
+// });
 
 app.get("/", (req, res) => {
   res.send("This is server");
